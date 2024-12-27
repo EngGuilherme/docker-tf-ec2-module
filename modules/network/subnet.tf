bbfb7b5f425f2803e0c65a5330921b53a397f822
Guilherme Moreno Vieira <enggmvieira@gmail.com>
2024-12-27 18:28:34 -0300

    feat: Updating subnet

--- modules/network/subnet.tf
+++ modules/network/subnet.tf
@@ -4,8 +4,9 @@ resource "aws_subnet" "main_subnet" {
   count                   = var.instance_count
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, count.index)
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
   map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index % length(data.aws_availability_zones.available.names)]
+
 
   tags = merge(
 
